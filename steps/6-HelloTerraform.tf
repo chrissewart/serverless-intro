98cd56b195ac06f510d45317dd6e81e6eb741bbb
Chris Sewart <chris.sewart@coopdigital.co.uk>
2018-05-23 15:21:20 +0100

    Added root path handling

--- steps/6-HelloTerraform.tf
+++ steps/6-HelloTerraform.tf
@@ -66,6 +66,7 @@ resource "aws_api_gateway_resource" "proxy" {
   path_part   = "{proxy+}"
 }
 
+# Method repeated twice. Once for / and once for /*
 resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = "${aws_api_gateway_rest_api.helloapitf.id}"
   resource_id   = "${aws_api_gateway_resource.proxy.id}"
@@ -73,10 +74,28 @@ resource "aws_api_gateway_method" "proxy" {
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = "${aws_api_gateway_rest_api.helloapitf.id}"
+  resource_id   = "${aws_api_gateway_rest_api.helloapitf.root_resource_id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+# Integration also repeated twice, once for / method and once for /* mathod
 resource "aws_api_gateway_integration" "lambda" {
   rest_api_id = "${aws_api_gateway_rest_api.helloapitf.id}"
   resource_id = "${aws_api_gateway_method.proxy.resource_id}"
   http_method = "${aws_api_gateway_method.proxy.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.helloapitf.invoke_arn}"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = "${aws_api_gateway_rest_api.helloapitf.id}"
+  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -95,7 +114,7 @@ resource "aws_lambda_permission" "apigw" {
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.helloapitf.arn}"
   principal     = "apigateway.amazonaws.com"
-  source_arn = "${aws_api_gateway_deployment.helloapitf.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_deployment.helloapitf.execution_arn}/*"
 }
 
 # Print out the URL 
